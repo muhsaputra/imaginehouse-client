bd6d7a58bf48fda92e89fdfbb3aa9ddb78040f93
muhsaputra <muhsaputrabiz@gmail.com>
2025-08-12 02:41:35 +0700

    fix: correct import path for sanityClient

--- src/temp_sanity_client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@sanity/client";
-
-export const client = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID, // ganti dari sanity.json / manage.sanity.io
-  dataset: import.meta.env.VITE_SANITY_DATASET,
-  apiVersion: "2025-07-01", // bulan dan hari cukup sesuai // pakai tanggal hari ini
-  useCdn: true,
-});
