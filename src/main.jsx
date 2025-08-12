82c84cbd88f512ee691295d0cb15c20c84e1eab1
muhsaputra <muhsaputrabiz@gmail.com>
2025-08-13 02:37:37 +0700

    updated store in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux"; // ⬅️ tambahkan ini
+import { store } from "./dashboard/store/store.js"; // ⬅️ sesuaikan path store-mu
 import "./index.css";
-import App from "./App.jsx"; // kalau App.jsx ada di /src
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      {" "}
+      {/* ⬅️ Bungkus seluruh app */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
